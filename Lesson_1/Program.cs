96c1a37aa1254f27e0db689fe24c88ac6852caf7
san27sin <sin27san@gmail.com>
2022-05-03 16:22:12 +0300

    Lesson_1

---
+++ Lesson_1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Lesson_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Как тебя зовут?\t");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Привет, {name}, сегодня {DateTime.Now}.");
+        }
+    }
+}
