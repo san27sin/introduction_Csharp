00771102e6779541397b063574201d0c45aa7d60
Alexander <92460691+san27sin@users.noreply.github.com>
2022-06-07 22:59:23 +0300

    Add files via upload

---
+++ Lesson_7/Lesson_7edit/Program.cs
@@ -0,0 +1,22 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: Lesson_7.Program
+// Assembly: Lesson_7, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: AD345A92-D4A9-4809-9F0A-90AA64D219C3
+// Assembly location: C:\Users\sin27\source\repos\Lesson_7\bin\Release\net5.0\Lesson_7.dll
+
+using System;
+
+namespace Lesson_7
+{
+  internal class Program
+  {
+    private static void Main(string[] args)
+    {
+      Console.WriteLine("What's your name?");
+      string str = Console.ReadLine();
+      Console.WriteLine("Hello " + str + "!");
+      int num = str == "sasha" ? 1 : 0;
+      Console.ReadKey(true);
+    }
+  }
+}
