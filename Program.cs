733a984ffa18bd2f2962e1319f11fc52b329cfd7
Alexander <92460691+san27sin@users.noreply.github.com>
2022-06-11 10:28:01 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Properties.Settings.Default.greeting);
+            if(string.IsNullOrEmpty(Properties.Settings.Default.name))
+            {
+                Console.Write("Введите имя: ");
+                Properties.Settings.Default.name = Console.ReadLine();
+                Console.Write("\nВведите возраст: ");
+                Properties.Settings.Default.age = int.Parse(Console.ReadLine());
+                Console.Write("\nВведите род деятельности: ");
+                Properties.Settings.Default.activity = Console.ReadLine();
+                Properties.Settings.Default.Save();
+            }
+            else
+            {
+                Console.WriteLine($"Имя:{Properties.Settings.Default.name}\nВозраст:{Properties.Settings.Default.age}\nРод деятельности:{Properties.Settings.Default.activity}");
+            }
+            Console.ReadKey(true);        }
+    }
+}
